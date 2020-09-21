6324b65bd095565e0939823ac9859093c2beb958
Sampath Bandla <36627290+sampathbandla@users.noreply.github.com>
2020-09-21 15:27:36 +0530

    Updated App with Routes and Header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/prop-types */
+import React from "react";
+import "./App.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Index from "./pages";
+import Dashboard from "./pages/dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+	return (
+		<Router>
+			<div className="App">
+				<Switch>
+					<Route path="/" exact component={Index} />
+					<Route path="/" component={Dashboard} />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
